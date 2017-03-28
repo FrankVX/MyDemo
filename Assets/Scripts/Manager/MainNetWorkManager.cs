4c8f19e9a50e2d75d7b94198bf244a376fa8b017
FrankVX <575163384@qq.com>
2017-03-28 22:29:49 +0800

    1

--- Assets/Scripts/Manager/MainNetWorkManager.cs
+++ Assets/Scripts/Manager/MainNetWorkManager.cs
@@ -62,7 +62,6 @@ public class MainNetWorkManager : NetworkManager
         ClientScene.Ready(conn);
         //if (!NetworkServer.active) ClientScene.AddPlayer(0);
         ClientScene.AddPlayer(0);
-
     }
 
 }
